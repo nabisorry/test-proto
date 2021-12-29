5a6dd32c748ee9d20402b526ca02d9a0b5301d18
nabisorry <chdb57@naver.com>
2021-12-29 12:39:08 +0900

    wip

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,11 @@
-import { ClinentMessage, ServerMessage } from 'src/proto/pii_pb';
 import { PackageDefinition, Options } from '@grpc/proto-loader';
-import * as E from '../proto/pii_grpc_pb';
+import * as P from '../proto/pii_grpc_pb';
+import * as B from '../proto/pii_pb';
 
 import * as I from '../index';
 
-export { ClinentMessage, ServerMessage };
 export function createProtoPackage(path: './src/proto/pii.proto', options: Options): PackageDefinition;
 export declare class PIIServer extends I.PIIServer {}
-export declare class GrpcStreamClient extends E.GrpcStreamClient {}
+export declare class GrpcStreamClient extends P.GrpcStreamClient {}
+export declare class ClinentMessage extends B.ClinentMessage {}
+export declare class ServerMessage extends B.ServerMessage {}
