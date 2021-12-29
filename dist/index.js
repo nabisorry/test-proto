3c4aaab580210707f912169ea4d05a4013984321
nabisorry <chdb57@naver.com>
2021-12-29 12:34:36 +0900

    wip

--- dist/index.js
+++ dist/index.js
@@ -1,75 +1,16 @@
-'use strict';
+"use strict";
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.GrpcStreamService = exports.GrpcStreamClient = exports.PIIServer = void 0;
-// import * as grpc from '@grpc/grpc-js';
-// import * as protoLoader from '@grpc/proto-loader';
-// import { GrpcStreamClient } from './proto/pii_grpc_pb';
-// const PROTO_PATH = './src/proto/pii.proto';
-// export interface GrpcAcessorOption {
-//     protoPath: string;
-//     protoLoaderOption: protoLoader.Options;
-// }
-// const DEFAULT_OPTIONS: GrpcAcessorOption = {
-//     protoPath: './src/proto/pii.proto',
-//     protoLoaderOption: {
-//         keepCase: true,
-//         longs: String,
-//         enums: String,
-//         defaults: true,
-//         oneofs: true,
-//     },
-// };
-// class GrpcController {
-//     packageDefinition: protoLoader.PackageDefinition;
-//     protoDescriptor: any;
-//     constructor(options = DEFAULT_OPTIONS) {
-//         const { protoPath, protoLoaderOption } = options;
-//         this.packageDefinition = protoLoader.loadSync(protoPath, protoLoaderOption);
-//         this.protoDescriptor = grpc.loadPackageDefinition(this.packageDefinition);
-//     }
-//     serverStart(bindAddress = 'localhost:7777') {
-//         const server = new grpc.Server();
-//         // @ts-ignore
-//         server.addService(GrpcStreamService, new PIIServer());
-//         server.bindAsync(bindAddress, grpc.ServerCredentials.createInsecure(), (err, port) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             console.log(`Listening on ${port}`);
-//             server.start();
-//         });
-//         return server;
-//     }
-// }
-// const ab = new GrpcController();
-// const a = ab.serverStart();
-// export const createProtoPackage = (options = DEFAULT_OPTIONS): protoLoader.PackageDefinition => {
-//     return protoLoader.loadSync(PROTO_PATH, options);
-// };
-var index_1 = require('./server/index');
-Object.defineProperty(exports, 'PIIServer', {
-    enumerable: true,
-    get: function () {
-        return __importDefault(index_1).default;
-    },
-});
-var pii_grpc_pb_1 = require('../src/proto/pii_grpc_pb');
-Object.defineProperty(exports, 'GrpcStreamClient', {
-    enumerable: true,
-    get: function () {
-        return pii_grpc_pb_1.GrpcStreamClient;
-    },
-});
-Object.defineProperty(exports, 'GrpcStreamService', {
-    enumerable: true,
-    get: function () {
-        return pii_grpc_pb_1.GrpcStreamService;
-    },
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GrpcStreamService = exports.GrpcStreamClient = exports.ServerMessage = exports.ClinentMessage = exports.PIIServer = void 0;
+var index_1 = require("./server/index");
+Object.defineProperty(exports, "PIIServer", { enumerable: true, get: function () { return __importDefault(index_1).default; } });
+var pii_pb_1 = require("./proto/pii_pb");
+Object.defineProperty(exports, "ClinentMessage", { enumerable: true, get: function () { return pii_pb_1.ClinentMessage; } });
+Object.defineProperty(exports, "ServerMessage", { enumerable: true, get: function () { return pii_pb_1.ServerMessage; } });
+var pii_grpc_pb_1 = require("./proto/pii_grpc_pb");
+Object.defineProperty(exports, "GrpcStreamClient", { enumerable: true, get: function () { return pii_grpc_pb_1.GrpcStreamClient; } });
+Object.defineProperty(exports, "GrpcStreamService", { enumerable: true, get: function () { return pii_grpc_pb_1.GrpcStreamService; } });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-// import * as grpc from '@grpc/grpc-js';
-// import * as protoLoader from '@grpc/proto-loader';
-// import { GrpcStreamClient } from './proto/pii_grpc_pb';
-
-// const PROTO_PATH = './src/proto/pii.proto';
-// export interface GrpcAcessorOption {
-//     protoPath: string;
-//     protoLoaderOption: protoLoader.Options;
-// }
-
-// const DEFAULT_OPTIONS: GrpcAcessorOption = {
-//     protoPath: './src/proto/pii.proto',
-//     protoLoaderOption: {
-//         keepCase: true,
-//         longs: String,
-//         enums: String,
-//         defaults: true,
-//         oneofs: true,
-//     },
-// };
-
-// class GrpcController {
-//     packageDefinition: protoLoader.PackageDefinition;
-//     protoDescriptor: any;
-
-//     constructor(options = DEFAULT_OPTIONS) {
-//         const { protoPath, protoLoaderOption } = options;
-//         this.packageDefinition = protoLoader.loadSync(protoPath, protoLoaderOption);
-//         this.protoDescriptor = grpc.loadPackageDefinition(this.packageDefinition);
-//     }
-
-//     serverStart(bindAddress = 'localhost:7777') {
-//         const server = new grpc.Server();
-//         // @ts-ignore
-//         server.addService(GrpcStreamService, new PIIServer());
-//         server.bindAsync(bindAddress, grpc.ServerCredentials.createInsecure(), (err, port) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             console.log(`Listening on ${port}`);
-//             server.start();
-//         });
-//         return server;
-//     }
-// }
-
-// const ab = new GrpcController();
-
-// const a = ab.serverStart();
-
-// export const createProtoPackage = (options = DEFAULT_OPTIONS): protoLoader.PackageDefinition => {
-//     return protoLoader.loadSync(PROTO_PATH, options);
-// };
 export { default as PIIServer } from './server/index';
-export { GrpcStreamClient, GrpcStreamService } from 'src/proto/pii_grpc_pb';
+export { ClinentMessage, ServerMessage } from './proto/pii_pb';
+export { GrpcStreamClient, GrpcStreamService } from './proto/pii_grpc_pb';
