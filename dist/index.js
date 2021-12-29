7b0104d301088d6721ae518f431f9432d225bbad
nabisorry <chdb57@naver.com>
2021-12-29 13:43:08 +0900

    wip

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.GrpcStreamService = exports.GrpcStreamClient = exports.ServerMessage = exports.ClinentMessage = exports.PIIServer = void 0;
+exports.GrpcStreamService = exports.GrpcStreamClient = exports.ServerMessage = exports.ClientMessage = exports.PIIServer = void 0;
 var index_1 = require("./server/index");
 Object.defineProperty(exports, "PIIServer", { enumerable: true, get: function () { return __importDefault(index_1).default; } });
 var pii_pb_1 = require("./proto/pii_pb");
-Object.defineProperty(exports, "ClinentMessage", { enumerable: true, get: function () { return pii_pb_1.ClinentMessage; } });
+Object.defineProperty(exports, "ClientMessage", { enumerable: true, get: function () { return pii_pb_1.ClientMessage; } });
 Object.defineProperty(exports, "ServerMessage", { enumerable: true, get: function () { return pii_pb_1.ServerMessage; } });
 var pii_grpc_pb_1 = require("./proto/pii_grpc_pb");
 Object.defineProperty(exports, "GrpcStreamClient", { enumerable: true, get: function () { return pii_grpc_pb_1.GrpcStreamClient; } });
