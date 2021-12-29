57429ef436f87d1b52334a7954d51e2cfbdc7d76
nabisorry <chdb57@naver.com>
2021-12-29 11:24:43 +0900

    wip

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,8 +5,8 @@ import { IGrpcStreamServer } from 'src/proto/pii_grpc_pb';
 import { ClinentMessage, ServerMessage } from 'src/proto/pii_pb';
 
 class PIIServer implements IGrpcStreamServer {
-    stream(call: grpc.ServerDuplexStream<ClinentMessage, ServerMessage>): void {
-        throw 'override!';
+    stream(call: grpc.ServerDuplexStream<ClinentMessage, ServerMessage>): null {
+        return null;
     }
 }
 
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,9 @@
 import { ClinentMessage, ServerMessage } from 'src/proto/pii_pb';
+import { PackageDefinition, Options } from '@grpc/proto-loader';
+import { GrpcStreamClient } from '../proto/pii_grpc_pb';
+
 import * as I from '../index';
 
-export { ClinentMessage, ServerMessage };
+export { ClinentMessage, ServerMessage, GrpcStreamClient };
+export function createProtoPackage(path: './src/proto/pii.proto', options: Options): PackageDefinition;
 export declare class PIIServer extends I.PIIServer {}
