67a27a67be3ba8b71eb6c60b2713e3740107360a
nabisorry <chdb57@naver.com>
2021-12-29 12:14:03 +0900

    wip

---
+++ dist/server/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class PIIServer {
+    stream(call) {
+        return null;
+    }
+}
+exports.default = PIIServer;
+//# sourceMappingURL=index.js.map
